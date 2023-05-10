95ce0fba21c185e675f8146d8722d55b9bf11bca
ElenaCKay <elenakay1@googlemail.com>
2023-05-10 09:50:18 +0100

    Navigation fixed

--- App.tsx
+++ App.tsx
@@ -1,28 +1,10 @@
 import "./config/firebase";
-import Tab from "./shared/Tabs";
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import WelcomeScreen from "./screens/Welcome";
-import { useAuthentication } from "./utils/useAuthentication";
+
+import RootNavigation from "./navigation";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-	const { user } = useAuthentication();
-
-	return (
-		<NavigationContainer>
-			{!user ? (
-				<Stack.Navigator
-					screenOptions={{
-						headerShown: false,
-					}}
-				>
-					<Stack.Screen name="Welcome" component={WelcomeScreen} />
-				</Stack.Navigator>
-			) : (
-				<Tab />
-			)}
-		</NavigationContainer>
-	);
+    return <RootNavigation />;
 }
--- navigation/authStack.tsx
+++ navigation/authStack.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import WelcomeScreen from "../screens/Welcome";
-import SignInScreen from "../screens/SignInScreen";
-import SignOutScreen from "../screens/SignUpScreen";
-import UserProfile from "../screens/UserProfile"
+import UserProfile from "../screens/UserProfile";
 import Tab from "../shared/Tabs";
 const Stack = createStackNavigator();
 
 export default function AuthStack() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Welcome" component={Tab} />
-				<Stack.Screen name="Sign In" component={SignInScreen} />
-				<Stack.Screen name="Sign Up" component={SignOutScreen} />
-				<Stack.Screen name="User Profile" component={UserProfile} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Welcome" component={Tab} />
+                <Stack.Screen name="User Profile" component={UserProfile} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
