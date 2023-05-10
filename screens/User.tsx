58f3888e63a2e16fd67f91cfdab36dddcae43efe
ElenaCKay <elenakay1@googlemail.com>
2023-05-10 13:24:35 +0100

    Completed profile still needs styling

--- screens/User.tsx
+++ screens/User.tsx
@@ -1,111 +1,128 @@
 import { getAuth, signOut, updateProfile } from "firebase/auth";
 import React, { useCallback, useEffect, useState } from "react";
 import {
-	StyleSheet,
-	Text,
-	View,
-	Image,
-	Button,
-	TouchableOpacity,
-	ScrollView,
-	Dimensions,
-	NativeScrollEvent,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Button,
+    TouchableOpacity,
+    ScrollView,
+    Dimensions,
+    NativeScrollEvent,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { catmarkers } from "../assets/catmarkers/catmarkers";
-
+import useUserProfile from "../utils/hooks/useUserProfile";
 // &#x25cf; - big dot
 
 const auth = getAuth();
 const { width, height } = Dimensions.get("window");
 
 export default function UserScreen() {
-	const [galleryPerm, setGalleryPerm] = useState(null);
-	const [image, setImage] = useState(catmarkers[1]);
-	const [active, setActive] = useState(0);
-	const user = auth.currentUser;
+    const [galleryPerm, setGalleryPerm] = useState(null);
+    const [image, setImage] = useState(catmarkers[1]);
+    const [active, setActive] = useState(0);
+    const user = auth.currentUser;
+    const username = user.displayName;
 
-	useEffect(() => {
-		(async () => {
-			const galleryStatus =
-				await ImagePicker.requestMediaLibraryPermissionsAsync();
-			setGalleryPerm(galleryStatus.status === "granted");
-		})();
-	}, []);
+    useEffect(() => {
+        (async () => {
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setGalleryPerm(galleryStatus.status === "granted");
+        })();
+    }, []);
 
-	const pickImage = async () => {
-		const result = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.Images,
-			allowsEditing: true,
-			aspect: [4, 3],
-			base64: true,
-		});
-		if (!result.canceled) {
-			setImage("data:image/png;base64," + result.assets[0].base64);
-		}
-	};
-	if (galleryPerm === false) {
-		return <Text>No access to storage</Text>;
-	}
+    const pickImage = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            base64: true,
+        });
+        if (!result.canceled) {
+            setImage("data:image/png;base64," + result.assets[0].base64);
+        }
+    };
 
-	const onchange = (nativeEvent: NativeScrollEvent) => {
+    if (galleryPerm === false) {
+        return <Text>No access to storage</Text>;
+    }
+
+    const userProfileState = useUserProfile(username);
+
+    if (userProfileState.isError) {
+        return <Text>Something Went Wrong!</Text>;
+    }
+
+    if (userProfileState.isLoading) {
+        return (
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
+
+    const userProfile = userProfileState.userProfile;
+
+    console.log(userProfile, "I AM THE USER PROFILE!!!!!");
+
+    const onchange = (nativeEvent: NativeScrollEvent) => {
         const { contentOffset, layoutMeasurement } = nativeEvent;
         if (contentOffset) {
             const slide = Math.ceil(contentOffset.x / layoutMeasurement.width);
             if (slide !== active) {
                 setActive(slide);
             }
-		}
-	};
+        }
+    };
 
-	return (
-		<View tw="flex-1 items-center m-3">
-			<Text className=" text-3xl font-bold m-3">Welcome {user?.displayName}!</Text>
-			<TouchableOpacity onPress={pickImage}>
-				<Image source={{ uri: image }} tw="h-72 w-72 rounded-3xl" />
-			</TouchableOpacity>
-			<Text tw="text-xl mt-3">Cats:</Text>
-			<ScrollView
-				style={{ width: width, height: height * 0.35 }}
-				onScroll={({ nativeEvent }) => onchange(nativeEvent)}
-				pagingEnabled
-				showsHorizontalScrollIndicator={false}
-				horizontal
-			>
-				{catmarkers.map((e, index) => (
-					<View
-						key={e}
-						tw="flex-row align-middle justify-center"
-						style={{ width: width }}
-					>
-						<View tw="flex-col text-center">
-							<Image source={{ uri: e }} tw="h-60 w-72" />
-							<View>
-								<Text tw="text-center">Name:</Text>
-								<Text tw="text-center">Age:</Text>
-								<Text tw="text-center">Breed:</Text>
-								<Text tw="text-center">Characteristics:</Text>
-							</View>
-						</View>
-					</View>
-				))}
-				<View tw="bottom-0 flex-row align-middle absolute"></View>
-			</ScrollView>
-			<View tw="flex-row bottom-9 self-center absolute">
-				{catmarkers.map((e, index) => (
-					<Text
-						key={e}
-						style={
-							active === index
-								? { color: "grey", margin: 3 }
-								: { color: "black", margin: 3 }
-						}
-					>
-						&#x25cf;
-					</Text>
-				))}
-			</View>
-			<Button title="Sign Out" onPress={() => signOut(auth)} />
-		</View>
-	);
+    return (
+        <View tw="flex-1 items-center m-3">
+            <Text className=" text-3xl font-bold m-3">Welcome {user?.displayName}!</Text>
+            <TouchableOpacity onPress={pickImage}>
+                <Image source={{ uri: userProfile.avatar }} tw="h-72 w-72 rounded-3xl" />
+            </TouchableOpacity>
+            <Text tw="text-xl mt-3">Cats:</Text>
+            <ScrollView
+                style={{ width: width, height: height * 0.35 }}
+                onScroll={({ nativeEvent }) => onchange(nativeEvent)}
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                horizontal
+            >
+                {userProfile.cats.length === 0 ? (
+                    <View>
+                        <Text>This user has no cats</Text>
+                    </View>
+                ) : (
+                    userProfile.cats.map((e, index) => (
+                        <View key={e} tw="flex-row align-middle justify-center" style={{ width: width }}>
+                            <View tw="flex-col text-center">
+                                <Image source={{ uri: e }} tw="h-60 w-72" />
+                                <View>
+                                    <Text tw="text-center">Name:</Text>
+                                    <Text tw="text-center">Age:</Text>
+                                    <Text tw="text-center">Breed:</Text>
+                                    <Text tw="text-center">Characteristics:</Text>
+                                </View>
+                            </View>
+                        </View>
+                    ))
+                )}
+                <View tw="bottom-0 flex-row align-middle absolute"></View>
+            </ScrollView>
+            <View tw="flex-row bottom-9 self-center absolute">
+                {userProfile.cats.map((e, index) => (
+                    <Text
+                        key={e}
+                        style={active === index ? { color: "grey", margin: 3 } : { color: "black", margin: 3 }}
+                    >
+                        &#x25cf;
+                    </Text>
+                ))}
+            </View>
+            <Button title="Sign Out" onPress={() => signOut(auth)} />
+        </View>
+    );
 }
--- screens/UserProfile.tsx
+++ screens/UserProfile.tsx
@@ -1,11 +1,14 @@
-import {StyleSheet,
-	Text,
-	View,
-	Image,
-	Button,
-	TouchableOpacity,
-	ScrollView,
-	Dimensions, NativeScrollEvent } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Button,
+    TouchableOpacity,
+    ScrollView,
+    Dimensions,
+    NativeScrollEvent,
+} from "react-native";
 import React, { useState } from "react";
 import useUserProfile from "../utils/hooks/useUserProfile";
 
@@ -15,61 +18,62 @@ export default function UserProfile({ route }) {
     const [active, setActive] = useState(0);
     const { width, height } = Dimensions.get("window");
 
-   const onchange = (nativeEvent: NativeScrollEvent) => {
+    const onchange = (nativeEvent: NativeScrollEvent) => {
         const { contentOffset, layoutMeasurement } = nativeEvent;
         if (contentOffset) {
             const slide = Math.ceil(contentOffset.x / layoutMeasurement.width);
             if (slide !== active) {
                 setActive(slide);
             }
-		}
-	};
+        }
+    };
 
     if (isError) return <Text>Something Went Wrong!</Text>;
 
-    if(isLoading){
-        return (<View>
+    if (isLoading) {
+        return (
+            <View>
                 <Text>Loading...</Text>
-            </View>)
-    };
+            </View>
+        );
+    }
 
-    return (<View tw="flex-1 items-center">
-                <Image source={{ uri: userProfile!.avatar }} style={{ width:300, height: 300 }} />
-                <Text tw="text-2xl">{userProfile!.username}</Text>
-                <Text>{userProfile!.description}</Text>
-                <Text tw="text-xl mt-3">Cats:</Text>
-                <ScrollView
-                    style={{ width: width, height: height * 0.35 }}
-                    onScroll={({ nativeEvent }) => onchange(nativeEvent)}
-                    pagingEnabled
-                    showsHorizontalScrollIndicator={false}
-                    horizontal
-                >
-		{userProfile.cats.length === 0 ? <View >
-                        <Text tw="text-center">
-                            No Cats Available...
-                        </Text>
-                </View>
-         :
-        userProfile!.cats.map((cats, index) => (
-					<View
-						key={cats.cat_id}
-						tw="flex-row align-middle justify-center"
-						style={{ width: width }}
-					>
-						<View tw="flex-col text-center">
-							<Image source={{ uri: cats.cat_img }} tw="h-60 w-72" />
-							<View>
-								<Text tw="text-center">Name: {cats.cat_name}</Text>
-								<Text tw="text-center">Age: {cats.age}</Text>
-								<Text tw="text-center">Breed: {cats.breed}</Text>
-								<Text tw="text-center">Top 2 Characteristics: {cats.characteristics[0]}, {cats.characteristics[1]}</Text>
-							</View>
-						</View>
-					</View>
-				))}
-				<View tw="bottom-0 flex-row align-middle absolute"></View>
-			</ScrollView>
+    return (
+        <View tw="flex-1 items-center">
+            <Image source={{ uri: userProfile!.avatar }} style={{ width: 300, height: 300 }} />
+            <Text tw="text-2xl">{userProfile!.username}</Text>
+            <Text>{userProfile!.description}</Text>
+            <Text tw="text-xl mt-3">Cats:</Text>
+            <ScrollView
+                style={{ width: width, height: height * 0.35 }}
+                onScroll={({ nativeEvent }) => onchange(nativeEvent)}
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                horizontal
+            >
+                {userProfile.cats.length === 0 ? (
+                    <View>
+                        <Text tw="text-center">No Cats Available...</Text>
+                    </View>
+                ) : (
+                    userProfile!.cats.map((cats, index) => (
+                        <View key={cats.cat_id} tw="flex-row align-middle justify-center" style={{ width: width }}>
+                            <View tw="flex-col text-center">
+                                <Image source={{ uri: cats.cat_img }} tw="h-60 w-72" />
+                                <View>
+                                    <Text tw="text-center">Name: {cats.cat_name}</Text>
+                                    <Text tw="text-center">Age: {cats.age}</Text>
+                                    <Text tw="text-center">Breed: {cats.breed}</Text>
+                                    <Text tw="text-center">
+                                        Top 2 Characteristics: {cats.characteristics[0]}, {cats.characteristics[1]}
+                                    </Text>
+                                </View>
+                            </View>
+                        </View>
+                    ))
+                )}
+                <View tw="bottom-0 flex-row align-middle absolute"></View>
+            </ScrollView>
         </View>
     );
 }
