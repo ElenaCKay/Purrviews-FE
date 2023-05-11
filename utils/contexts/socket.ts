7117121a71af99f3d31bcc7240c310153f888b40
HarCass <harry.jc.cass@gmail.com>
2023-05-11 09:45:33 +0100

    Fixing socket address

--- utils/contexts/socket.ts
+++ utils/contexts/socket.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 import { Socket, io } from "socket.io-client";
 
-export const socket = io('https://purrviews-api.onrender.com:10000', {autoConnect: true});
+export const socket = io('https://purrviews-api.onrender.com', {autoConnect: true});
 export const SocketContext = createContext(null);
